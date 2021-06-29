288c8b43be3534c08aaa40cad2440005a5c49ad9
MSI\yoghu <t.shouei20040720@gmail.com>
2021-06-29 17:00:22 +0900

    main関数の中身実装

--- GameProgramming/C_Practice10/02_Refactoring/main.cpp
+++ GameProgramming/C_Practice10/02_Refactoring/main.cpp
@@ -1,10 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "fighter.h"
 
 int main()
 {
+	Fighter nanashi;
 
-
+	nanashi.hp = 3;
+	nanashi.str = 4;
+	nanashi.mag = 8;
+	nanashi.tec = 3;
+	nanashi.spd = 10;
+	nanashi.def = 2;
+	nanashi.luc = 9;
+	nanashi.mde = 12;
 
 	system("pause");
 	return 0;
